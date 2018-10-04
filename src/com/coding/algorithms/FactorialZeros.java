67e6a15d6ed7dda38a9132cf5be5425ab18e3921
NecmiK <necmik@gmail.com>
2018-10-04 18:34:59 +0300

    algorithms

---
+++ src/com/coding/algorithms/FactorialZeros.java
@@ -0,0 +1,25 @@
+package com.coding.algorithms;
+
+public class FactorialZeros {
+	
+	public static void main(String[] args) {
+		System.out.println(getZeros(25));
+	}
+
+	static int getZeros(int num) {
+		if (num <5 ) return 0;
+		
+		int count = 0;
+		for (int i=5; i<=num; i*=5) {
+			if (i%5 == 0) {
+				int temp = i;
+				while(temp >= 5) {
+					count++;
+					temp = temp/5;
+				}
+			}
+		}
+		
+		return count;
+	}
+}
